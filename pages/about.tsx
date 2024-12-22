5497b2f2d00e6e91b168de406ec0498f88a37a32
Nick (Shark) Marino <asharksfishbowl@gmail.com>
2024-12-22 02:11:34 -0600

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -19,32 +19,32 @@ const About: NextPage = () => {
             </p>
             <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-8">
                 <div className="flex flex-col items-center">
-                    <Image src="/images/person1.png" alt="Person 1" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
+                    <Image src="/images/shark.webp" alt="Person 1" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
                     <h3 className="text-xl font-bold">Nick Marino</h3>
                     <p className="text-gray-400">Founder & Architech</p>
                 </div>
                 <div className="flex flex-col items-center">
-                    <Image src="/images/person2.jpg" alt="Person 2" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
+                    <Image src="/images/miguel.webp" alt="Person 2" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
                     <h3 className="text-xl font-bold">Miguel Del Campo</h3>
                     <p className="text-gray-400">Founder & Project Manager</p>
                 </div>
                 <div className="flex flex-col items-center">
-                    <Image src="/images/person3.jpg" alt="Person 3" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
-                    <h3 className="text-xl font-bold">Emily Johnson</h3>
+                    <Image src="/images/sara.webp" alt="Person 3" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
+                    <h3 className="text-xl font-bold">Leah Johnson</h3>
                     <p className="text-gray-400">Founder & Marketing Director</p>
                 </div>
                 <div className="flex flex-col items-center">
-                    <Image src="/images/person1.jpg" alt="Person 1" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
+                    <Image src="/images/steve.webp" alt="Person 1" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
                     <h3 className="text-xl font-bold">John Doe</h3>
                     <p className="text-gray-400">Creative Director</p>
                 </div>
                 <div className="flex flex-col items-center">
-                    <Image src="/images/person2.jpg" alt="Person 2" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
+                    <Image src="/images/jane.webp" alt="Person 2" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
                     <h3 className="text-xl font-bold">Jane Smith</h3>
                     <p className="text-gray-400">Lead Developer</p>
                 </div>
                 <div className="flex flex-col items-center">
-                    <Image src="/images/person3.jpg" alt="Person 3" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
+                    <Image src="/images/emily.webp" alt="Person 3" className="w-32 h-32 rounded-full mb-4 content" width="100" height="100"/>
                     <h3 className="text-xl font-bold">Emily Johnson</h3>
                     <p className="text-gray-400">Quality Assurance Director</p>
                 </div>
